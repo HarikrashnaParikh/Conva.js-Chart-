730fc10ec6371b37e3c085f0f8f4e422aff76150
Harikrashna <hparikh@argusoft.com>
2023-05-19 18:48:14 +0530

    Practice of Rect and circle

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef } from '@angular/core';
+import { Layer } from 'konva/lib/Layer';
+import { Stage } from 'konva/lib/Stage';
+import { Circle } from 'konva/lib/shapes/Circle';
+import { Rect } from 'konva/lib/shapes/Rect';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +11,36 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'konva-chart-practice';
+  constructor(private elementRef: ElementRef){}
+  ngAfterViewInit() {
+    const stage = new Stage({
+      container: 'stage-container',
+      width: window.innerWidth,
+      height: window.innerHeight, 
+    });
+
+    const layer = new Layer();
+    stage.add(layer);
+
+    const rect = new Rect({
+      x: 50,
+      y: 50,
+      width: 100,
+      height: 100,
+      fill: 'red',
+      draggable: true
+    });
+    layer.add(rect);
+
+    const circle = new Circle({
+      x: 200,
+      y: 200,
+      radius: 50,
+      fill: 'blue',
+      draggable: true
+    });
+    layer.add(circle);
+
+    layer.draw();
+  }
 }
